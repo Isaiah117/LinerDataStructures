71ca2e85b4d79c3dac4de413f9c7639af60fd1c4
Isaiah117 <61758194+Isaiah117@users.noreply.github.com>
2020-04-28 21:24:14 -0400

    Add files via upload

---
+++ LinearDataStructure/QueueTLinearDataStructre/QueueTLinearDataStructre/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    private const int NUMBER_OF_MEMBERS = 50;
+    private const int COUNTER = 17;
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Queue<int> sequenceMembers = new Queue<int>();
+        sequenceMembers.Enqueue(n);
+
+        for (int i = 1; i <= NUMBER_OF_MEMBERS; i++)
+        {
+            int number = sequenceMembers.Dequeue();
+            Console.WriteLine("S{0} = {1}", i, number);
+
+           
+            if (i <= COUNTER)
+            {
+                sequenceMembers.Enqueue(number + 1);
+                sequenceMembers.Enqueue((2 * number) + 1);
+                sequenceMembers.Enqueue(number + 2);
+            }
+        }
+    }
+}
